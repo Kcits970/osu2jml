f95ea37f5e6e10641a34934cf44c1eac8ab4de50
Kcits970 <geometricstick@gmail.com>
2023-11-09 23:18:03 +0900

    changed the variable modifiers to public final for immutability.

--- src/math/Line2D.java
+++ src/math/Line2D.java
@@ -3,7 +3,7 @@ package math;
 public class Line2D {
     //represents the line ax + by + c = 0
 
-    double a, b, c;
+    public final double a, b, c;
 
     public Line2D(double a, double b, double c) {
         this.a = a;
--- src/math/PolarCoordinate.java
+++ src/math/PolarCoordinate.java
@@ -3,8 +3,8 @@ package math;
 import java.awt.geom.Point2D;
 
 public class PolarCoordinate {
-    public double r;
-    public double theta; //theta is in terms of radians.
+    public final double r;
+    public final double theta; //theta is in terms of radians.
 
     public PolarCoordinate(double r, double theta) {
         this.r = r;
@@ -12,9 +12,6 @@ public class PolarCoordinate {
     }
 
     public Point2D.Double toCartesian() {
-        return new Point2D.Double(
-                r * Math.cos(theta),
-                r * Math.sin(theta)
-        );
+        return new Point2D.Double(r * Math.cos(theta), r * Math.sin(theta));
     }
 }
