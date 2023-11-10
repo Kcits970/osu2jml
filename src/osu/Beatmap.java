ab89833668740101435a2364fa8a0a00ac4dc8e2
Kcits970 <geometricstick@gmail.com>
2023-11-11 00:48:49 +0900

    mass refactor part 2.

--- src/osu/Beatmap.java
+++ src/osu/Beatmap.java
@@ -4,101 +4,30 @@ import math.GeometryFunctions;
 
 import java.io.*;
 import java.util.*;
-import java.util.stream.Collectors;
+import java.util.function.Function;
+
+import static java.lang.Double.parseDouble;
 
 public class Beatmap {
-    private static final List<String> SECTION_NAMES;
-    static {
-        String[] sectionsArray = {
-                "[General]",
-                "[Editor]",
-                "[Metadata]",
-                "[Difficulty]",
-                "[Events]",
-                "[TimingPoints]",
-                "[Colours]",
-                "[HitObjects]"
-        };
-        SECTION_NAMES = List.of(sectionsArray);
-    }
-
-    public File originalFile;
-    public Map<String,List<String>> sections;
-    public List<TimingPoint> timingPoints;
     public List<HitObject> hitObjects;
 
     public double stackLeniency;
     public double approachRate;
     public double circleSize;
-    public double sliderMultiplier;
 
-    public Beatmap(File f) throws IOException {
-        originalFile = f;
-        sections = new HashMap<>();
-        timingPoints = new ArrayList<>();
+    public Beatmap(File f) throws Exception {
+        Map<String,List<String>> rawBeatmapData = readRawBeatmapData(f);
+
+        stackLeniency = parseDouble(findValue(rawBeatmapData, "[General]", "StackLeniency"));
+        approachRate = parseDouble(findValue(rawBeatmapData, "[Difficulty]", "ApproachRate"));
+        circleSize = parseDouble(findValue(rawBeatmapData, "[Difficulty]", "CircleSize"));
         hitObjects = new ArrayList<>();
 
-        setupSections();
-        setupGeneralParameters();
-        setupDifficultyParameters();
-        setupTimingPoints();
-        setupHitObjects();
-        calculateStackLayers();
-    }
+        double sliderMultiplier = parseDouble(findValue(rawBeatmapData, "[Difficulty]", "SliderMultiplier"));
 
-    void setupSections() throws IOException {
-        for (String s : SECTION_NAMES)
-            sections.put(s, new ArrayList<>());
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(originalFile)));
-        reader.readLine(); //skip the first line: "osu file format v14"
-
-        String currentSectionName = null;
-        for (String line = reader.readLine(); line != null; line = reader.readLine()) {
-            line = line.trim();
-
-            if (line.startsWith("//") || line.isBlank())
-                continue;
-            else if (line.startsWith("["))
-                currentSectionName = line;
-            else
-                sections.get(currentSectionName).add(line);
-        }
-
-        reader.close();
-    }
-
-    void setupGeneralParameters() {
-        List<String> generalStrings = sections.get("[General]");
-
-        for (String s : generalStrings) {
-            String generalTag = s.split(":")[0];
-            String generalValue = s.split(":")[1];
-
-            if (generalTag.startsWith("StackLeniency"))
-                stackLeniency = Double.parseDouble(generalValue);
-        }
-    }
-
-    void setupDifficultyParameters() {
-        List<String> difficultyStrings = sections.get("[Difficulty]");
-
-        for (String s : difficultyStrings) {
-            String difficultyTag = s.split(":")[0];
-            String difficultyValue = s.split(":")[1];
-
-            if (difficultyTag.startsWith("CircleSize"))
-                circleSize = Double.parseDouble(difficultyValue);
-            else if (difficultyTag.startsWith("ApproachRate"))
-                approachRate = Double.parseDouble(difficultyValue);
-            else if (difficultyTag.startsWith("SliderMultiplier"))
-                sliderMultiplier = Double.parseDouble(difficultyValue);
-        }
-    }
-
-    void setupTimingPoints() {
-        List<String> timingPointStrings = sections.get("[TimingPoints]");
-        List<TimingPoint> rawTimingPoints = timingPointStrings.stream().map(TimingPoint::new).toList();
+        //Reading the timing points from the raw beatmap data.
+        List<TimingPoint> rawTimingPoints = rawBeatmapData.get("[TimingPoints]").stream().map(TimingPoint::new).toList();
+        List<TimingPoint> timingPoints = new ArrayList<>();
 
         TimingPoint currentParent = null;
         for (TimingPoint rawTimingPoint : rawTimingPoints) {
@@ -110,20 +39,64 @@ public class Beatmap {
             else
                 timingPoints.add(new TimingPoint(rawTimingPoint, null));
         }
+
+        //Reading the hit objects from the raw beatmap data.
+        List<HitObject> rawHitObjects = rawBeatmapData.get("[HitObjects]").stream().map(HitObjectFactory::getHitObject).toList();
+
+        Function<HitObject,TimingPoint> timingPointFinder = hitObject ->
+                timingPoints.stream()
+                        .filter(timingPoint -> timingPoint.time <= hitObject.time)
+                        .max(Comparator.comparingDouble(timingPoint -> timingPoint.time))
+                        .get();
+
+        for (HitObject rawHitObject : rawHitObjects) {
+            if (rawHitObject instanceof Slider)
+                hitObjects.add(new Slider((Slider) rawHitObject, sliderMultiplier, timingPointFinder.apply(rawHitObject)));
+            else
+                hitObjects.add(rawHitObject);
+        }
+
+        calculateStackLayers();
     }
 
-    void setupHitObjects() {
-        List<String> hitObjectStrings = sections.get("[HitObjects]");
+    private static Map<String,List<String>> readRawBeatmapData(File f) throws Exception {
+        Map<String,List<String>> sections = new HashMap<>();
 
-        for (String s : hitObjectStrings)
-            hitObjects.add(HitObjectFactory.getHitObject(s));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(f)));
+        reader.readLine(); //skip the first line: "osu file format v14"
 
-        for (int i = 0; i < hitObjects.size(); i++) {
-            HitObject currentHitObject = hitObjects.get(i);
-            if (currentHitObject instanceof Slider) {
-                hitObjects.set(i, new Slider((Slider) currentHitObject, sliderMultiplier, BeatmapFunctions.findFurthestTimingPointUntil(timingPoints, currentHitObject.time)));
+        String currentSectionName = null;
+        for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+            line = line.trim();
+
+            if (line.startsWith("//") || line.isBlank())
+                continue;
+            else if (line.startsWith("[")) {
+                currentSectionName = line;
+                sections.putIfAbsent(currentSectionName, new ArrayList<>());
             }
+            else
+                sections.get(currentSectionName).add(line);
         }
+        reader.close();
+
+        return sections;
+    }
+
+    private static String findValue(Map<String,List<String>> rawBeatmapData, String sectionName, String attributeName) {
+        if (!rawBeatmapData.containsKey(sectionName))
+            throw new RuntimeException(String.format("Unable to find section name '%s'", sectionName));
+
+        Optional<String> possibleValue = rawBeatmapData.get(sectionName)
+                .stream()
+                .filter(string -> string.startsWith(attributeName))
+                .map(string -> string.substring(string.indexOf(':') + 1))
+                .findAny();
+
+        if (possibleValue.isEmpty())
+            throw new RuntimeException(String.format("Unable to find '%s' in '%s'", attributeName, sectionName));
+
+        return possibleValue.get();
     }
 
     void calculateStackLayers() {
